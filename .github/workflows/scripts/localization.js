da4b9d7664fea7d1f8b7f4c9f93d2ad8a98a25ce
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2020-02-02 00:14:57 +0530

    Test

--- .github/workflows/scripts/localization.js
+++ .github/workflows/scripts/localization.js
@@ -1,1 +1,29 @@
-console.log(process.env.argv)
+(async () => {
+  try {
+    // check for project keys
+    let { CROWDIN_KEY,  CROWDIN_PROJECT_NAME } = process.env;
+    if (!CROWDIN_KEY && !CROWDIN_PROJECT_NAME) {
+      throw new Error('Config keys missing');
+      // process.exit(1);
+    }
+
+    // create crowdin axios instance
+    const crowdin = axios.create({
+      baseURL: `https://api.crowdin.com/api/project/${process.env.CROWDIN_PROJECT_NAME}`,
+    });
+    crowdin.interceptors.request.use(function (config) {
+      config.url = `${config.url}/?key=${process.env.CROWDIN_KEY}&json=true`
+      return config;
+    });
+
+    // download proccess
+    // TODO: check git msg also
+    if (!process.env.SKIP_DOWNLOAD) {
+      const zip = await crowdin.get('info');
+      console.log(zip.data)
+    }
+
+  } catch(e) {
+    console.log(e)
+  }
+})()
