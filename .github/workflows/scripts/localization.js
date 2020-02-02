75388c7f5aae88edb863bf40acfb78803a04b7a8
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2020-02-02 20:24:52 +0530

    xs

--- .github/workflows/scripts/localization.js
+++ .github/workflows/scripts/localization.js
@@ -1,82 +1,74 @@
 const github = require('@actions/github');
-const core = require('@actions/core');
 
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-console.log(process.env.SKIP_DOWNLOAD)
-if (process.env.SKIP_DOWNLOAD) {
-  console.log('download')
+
+
+
+
+class Localization {
+  constructor () {
+     // check for project keys
+    let { CROWDIN_KEY, CROWDIN_PROJECT_NAME } = process.env;
+    if (!CROWDIN_KEY || !CROWDIN_PROJECT_NAME) {
+      console.error('Config keys missing.')
+      process.exit(1);
+    }
+    this.gitMessage = github.context.payload.head_commit.message;
+    console.log(this.gitMessage)
+    this.provider = this.createProviderInstance(CROWDIN_KEY, CROWDIN_PROJECT_NAME);
+    this.downloadFiles();
+  }
+
+  createProviderInstance(key, project) {
+    let provider = axios.create({
+      baseURL: `https://api.crowdin.com/api/project/${project}`,
+    });
+    provider.interceptors.request.use(function (config) {
+      config.url = `${config.url}/?key=${key}&json=true`
+      return config;
+    });
+    return provider;
+  }
+
+  async downloadFiles() {
+    let { SKIP_DOWNLOAD, SKIP_CROWDIN_BUILD } = process.env;
+
+    // TODO: check git msg also
+    let skipDownload = SKIP_DOWNLOAD;
+    skipDownload && console.log('Download skipped');
+
+    if (!skipDownload) {
+      console.log('Downloading files from provider...');
+      SKIP_CROWDIN_BUILD && console.log('Provider build skipped.');
+      let downloadUrl = (SKIP_CROWDIN_BUILD) ? '/download/all.zip' : '/export';
+      try {
+        let files = await this.provider.get(downloadUrl);
+        let zipPath = this.createZipFile(files.data);
+        console.log(zipPath)
+        console.log('Created')
+      } catch(e) {
+        console.log('❌ Downloading files failed');
+        console.log(e)
+      }
+    }
+  }
+
+  createZipFile(data) {
+    let dir = path.resolve('./tmp');
+    let zipPath = `${dir}/all.zip`;
+    !fs.existsSync(dir) && fs.mkdirSync(dir);
+    fs.writeFileSync(zipPath, data, { encoding: 'ascii'});
+    return zipPath;
+  }
 }
-// console.log(github.context)
-// console.log(core.getInput('hello'))
 
-
-// process.env.SKIP_DOWNLOAD = false;
-// process.env.SKIP_CROWDIN_BUILD=true;
-
-// class Localization {
-//   constructor () {
-//     console.log(process.env)
-//      // check for project keys
-//     let { CROWDIN_KEY, CROWDIN_PROJECT_NAME } = process.env;
-//     if (!CROWDIN_KEY || !CROWDIN_PROJECT_NAME) {
-//       console.error('Config keys missing.')
-//       process.exit(1);
-//     }
-
-//     this.provider = this.createProviderInstance(CROWDIN_KEY, CROWDIN_PROJECT_NAME);
-//     this.downloadFiles();
-//   }
-
-//   createProviderInstance(key, project) {
-//     let provider = axios.create({
-//       baseURL: `https://api.crowdin.com/api/project/${project}`,
-//     });
-//     provider.interceptors.request.use(function (config) {
-//       config.url = `${config.url}/?key=${key}&json=true`
-//       return config;
-//     });
-//     return provider;
-//   }
-
-//   async downloadFiles() {
-//     let { SKIP_DOWNLOAD, SKIP_CROWDIN_BUILD } = process.env;
-
-//     // TODO: check git msg also
-//     let skipDownload = SKIP_DOWNLOAD;
-//     skipDownload && console.log('Download skipped');
-
-//     if (!skipDownload) {
-//       console.log('Downloading files from provider...');
-//       SKIP_CROWDIN_BUILD && console.log('Provider build skipped.');
-//       let downloadUrl = (SKIP_CROWDIN_BUILD) ? '/download/all.zip' : '/export';
-//       try {
-//         let files = await this.provider.get(downloadUrl);
-//         let zipPath = this.createZipFile(files.data);
-//         console.log(zipPath)
-//         console.log('Created')
-//       } catch(e) {
-//         console.log('❌ Downloading files failed');
-//         console.log(e)
-//       }
-//     }
-//   }
-
-//   createZipFile(data) {
-//     let dir = path.resolve('./tmp');
-//     let zipPath = `${dir}/all.zip`;
-//     !fs.existsSync(dir) && fs.mkdirSync(dir);
-//     fs.writeFileSync(zipPath, data, { encoding: 'ascii'});
-//     return zipPath;
-//   }
-// }
-
-// // initiate
-// try {
-//   new Localization()
-// } catch(e) {
-//   console.log(e)
-//   console.error('Something went wrong');
-//   process.exit(1);
-// }
+// initiate
+try {
+  new Localization()
+} catch(e) {
+  console.log(e)
+  console.error('Something went wrong');
+  process.exit(1);
+}
