fbdd7e6ec909561f107378c6746ff74469f7d8d3
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2020-02-02 09:50:00 +0530

    class method

--- .github/workflows/scripts/localization.js
+++ .github/workflows/scripts/localization.js
@@ -1,31 +1,70 @@
 const axios = require('axios');
 
-(async () => {
-  try {
-    // check for project keys
+class Localization {
+  constructor () {
+     // check for project keys
     let { CROWDIN_KEY,  CROWDIN_PROJECT_NAME } = process.env;
     if (!CROWDIN_KEY || !CROWDIN_PROJECT_NAME) {
       console.error('Config keys missing.')
       process.exit(1);
     }
 
-    // create crowdin axios instance
-    const crowdin = axios.create({
-      baseURL: `https://api.crowdin.com/api/project/${process.env.CROWDIN_PROJECT_NAME}`,
+    this.crowdinProjectName = CROWDIN_PROJECT_NAME;
+    this.crowdinKey = CROWDIN_KEY;
+    this.provider = this.createProviderInstance();
+
+    this.downloadFiles();
+  }
+
+  createProviderInstance() {
+    let provider = axios.create({
+      baseURL: `https://api.crowdin.com/api/project/${CROWDIN_PROJECT_NAME}`,
     });
-    crowdin.interceptors.request.use(function (config) {
-      config.url = `${config.url}/?key=${process.env.CROWDIN_KEY}&json=true`
+    provider.interceptors.request.use(function (config) {
+      config.url = `${config.url}/?key=${CROWDIN_KEY}&json=true`
       return config;
     });
+    return provider;
+  }
 
-    // download proccess
-    // TODO: check git msg also
+  async downloadFiles() {
+     // TODO: check git msg also
     if (!process.env.SKIP_DOWNLOAD) {
       const zip = await crowdin.get('info');
       console.log(zip.data)
     }
-
-  } catch(e) {
-    console.log(e)
   }
-})()
+}
+
+// (async () => {
+//   try {
+//     // check for project keys
+//     let { CROWDIN_KEY,  CROWDIN_PROJECT_NAME } = process.env;
+//     if (!CROWDIN_KEY || !CROWDIN_PROJECT_NAME) {
+//       console.error('Config keys missing.')
+//       process.exit(1);
+//     }
+
+//     // create crowdin axios instance
+
+
+//     // download proccess
+//     // TODO: check git msg also
+//     if (!process.env.SKIP_DOWNLOAD) {
+//       const zip = await crowdin.get('info');
+//       console.log(zip.data)
+//     }
+
+//   } catch(e) {
+//     console.error('Something went wrong');
+//     process.exit(1);
+//   }
+// })()
+
+// initiate
+try {
+  new Localization()
+} catch(e) {
+  console.error('Something went wrong');
+  process.exit(1);
+}
